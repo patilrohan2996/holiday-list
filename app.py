c09cd02634fb3976978fc3a198b00e027a5db7c0
Rohan Patil <65501871+patilrohan2996@users.noreply.github.com>
2025-06-05 16:32:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from streamlit_calendar import calendar
 from datetime import datetime
+import json
 
 # Set page config
 st.set_page_config(page_title="Company Holiday Calendar", layout="wide", page_icon="🗓️")
